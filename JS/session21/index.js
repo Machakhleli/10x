e120ca7f7868a02dca5a0bf545fc4b19ee2a955d
Machakhleli <ushangikakhidze@gmail.com>
2025-03-16 04:22:06 +0400

    done js part of task and tring to make it visually better with html and scss

---
+++ JS/session21/index.js
@@ -0,0 +1,19 @@
+let powerOfGandalf = 0;
+let powerOfDumbldor = 0;
+
+
+powerOfGandalf += 10;
+powerOfDumbldor += 10;
+
+console.log("Gandalf's power is: " + powerOfGandalf);
+console.log("Dumbldor's power is: " + powerOfDumbldor);
+
+if (powerOfGandalf > powerOfDumbldor) {
+    console.log("Gandalf is more powerful");
+} else if (powerOfGandalf < powerOfDumbldor) {
+    console.log("Dumbldor is more powerful");
+} else {
+    console.log("Both are equally powerful");
+}
+
+
