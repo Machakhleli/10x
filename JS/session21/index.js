d24db411efeabf7eeca1466b23d8c81eb6a6fe5e
Machakhleli <ushangikakhidze@gmail.com>
2025-03-17 14:59:58 +0400

    done first task

--- JS/session21/index.js
+++ JS/session21/index.js
@@ -2,18 +2,33 @@ let powerOfGandalf = 0;
 let powerOfDumbldor = 0;
 
 
-powerOfGandalf += 10;
-powerOfDumbldor += 10;
+function comparision(){
+    console.log(`gandalf's power is: ${powerOfGandalf}, dumbldor's power is: ${powerOfDumbldor}`);
 
-console.log("Gandalf's power is: " + powerOfGandalf);
-console.log("Dumbldor's power is: " + powerOfDumbldor);
-
-if (powerOfGandalf > powerOfDumbldor) {
-    console.log("Gandalf is more powerful");
-} else if (powerOfGandalf < powerOfDumbldor) {
-    console.log("Dumbldor is more powerful");
-} else {
-    console.log("Both are equally powerful");
+        if(powerOfGandalf > powerOfDumbldor){
+            console.log("Gandalf is stronger than Dumbldor");
+        }else if(powerOfGandalf < powerOfDumbldor){
+            console.log("Dumbldor is stronger than Gandalf");
+        }else{
+            console.log("They are in same range of power");
+        }
 }
 
+document.querySelector(".decrease-gandalfp").addEventListener("click", function(){
+    powerOfGandalf -= 8;
+    comparision();
+})
 
+document.querySelector(".increase-gandalfp").addEventListener("click", function(){
+    powerOfGandalf += 10;
+    comparision();
+})
+document.querySelector(".decrease-dumbldorep").addEventListener("click", function(){
+    powerOfDumbldor -= 6;
+    comparision();
+})
+document.querySelector(".increase-dumbldorep").addEventListener("click", function(){
+    powerOfDumbldor += 8;
+    comparision();
+})
+comparision();
