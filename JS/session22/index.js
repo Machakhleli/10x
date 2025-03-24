a2752d87749e9ff559985eb27b833401595acb0d
Machakhleli <ushangikakhidze@gmail.com>
2025-03-25 03:57:45 +0400

    done session 22 homeworks

--- JS/session22/index.js
+++ JS/session22/index.js
@@ -7,10 +7,13 @@ function compareAttack(pokemon1, attack1, pokemon2, attack2) {
     let message = `${pokemon1}'s power: ${attack1}, ${pokemon2}'s power: ${attack2} `;
 
     if (attack1 > attack2) {
-        message += `${pokemon1} is stronger!`
+        message += `${pokemon1} is stronger!`;
+        console.log(message += `${pokemon1} is stronger!`)
     } else {
-        message += `${pokemon2} is stronger!`
+        message += `${pokemon2} is stronger!`;
+        console.log(message += `${pokemon2} is stronger!`)
     }
+
 
     resultForPokemon1.innerHTML += `<p>${message}</p>`;
 
@@ -19,6 +22,10 @@ function compareAttack(pokemon1, attack1, pokemon2, attack2) {
 compareAttack("Pikachu", 55, "Charizard", 84);
 
 compareAttack("Pikachu", 100, "Charizard", 70);
+
+compareAttack("pikachu", 100, "charizard", 500);
+
+
 
 
 
@@ -72,3 +79,76 @@ let shadowClone = count => {
     return count * 5;
 }
 console.log(shadowClone(10));
+
+//task 8. Sorting the Sorting Hat – Callback Functions
+
+function sortHouses() {
+    let rendomizer = Math.floor(Math.random() * 4);
+
+    if (rendomizer == 0) {
+        return "Gryffindor";
+    } else if (rendomizer == 1) {
+        return "Slytherin";
+    } else if (rendomizer == 2) {
+        return "Ravenclaw";
+    } else {
+        return "Hufflepuff";
+    }
+
+}
+function sortStudents(name, usho) {
+    return `${name} sorted in ${usho}`;
+
+}
+console.log(sortStudents("Harry", sortHouses()));
+
+//9. Infinity Gauntlet – Function Scope
+
+function snapFingers() {
+    let stones = 6;
+    function checkStones() {
+        if (stones == 6) {
+            console.log("Thanos has 6 stones. The universe trembles!");
+        } else {
+            console.log("Fuck you Thanos!:D");
+        }
+    }
+    checkStones();
+}
+console.log(snapFingers());
+
+// 10. Time-Turner – Function Hoisting
+
+turnBackTime();
+
+function turnBackTime() {
+    console.log("Going back in time");
+}
+
+// 11. Batman’s Secret Identity – Function Closures
+
+function batman() {
+
+    function hiddenBatman() {
+        console.log("I am Batman");
+    }
+    return hiddenBatman;
+
+}
+let bruceWayne = batman();
+bruceWayne();
+
+// 12. The Enchanted Mirror – Higher-Order Functions
+
+function magicMirror(message, transformFunction) {
+    function transformFunction() {
+        let transformedMessage = message.toUpperCase();
+        return `The mirror says: ${transformedMessage}`;
+    }
+    console.log(transformFunction(message));
+
+}
+
+magicMirror("will I be rich?");
+magicMirror("am I cool?");
+magicMirror("of couerse you are cool )))")
