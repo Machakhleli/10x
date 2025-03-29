2524f2dc87bebcfb981f41d8888bc00ed96778ea
Machakhleli <ushangikakhidze@gmail.com>
2025-03-29 12:39:57 +0400

    done session 24's 3 task

--- JS/session24/index.js
+++ JS/session24/index.js
@@ -17,10 +17,30 @@ function compareBmi(marksMass, marksHeight, johnsMass, johnsHeight) {
         console.log(`Mark has ${marksBmi} and John has ${johnsBmi}. So Mark has lower BMI then John`);
     }
 }
-compareBmi(78, 1.69, 92, 1.95);ებ 
+compareBmi(78, 1.69, 92, 1.95);
 compareBmi(95, 1.88, 85, 1.76);
 
 
 // task 3 
+
+
+function compareAvarageScores(dolphinsScore1, dolphinsScore2, dolphinsScore3, koalasScore1, koalasScore2, koalasScore3) {
+
+    let dolphinsAvarageScore = Math.floor((dolphinsScore1 + dolphinsScore2 + dolphinsScore3) / 3);
+
+    let koalasAvarageScore = Math.floor((koalasScore1 + koalasScore2 + koalasScore3) / 3);
+
+    if (dolphinsAvarageScore > koalasAvarageScore) {
+        console.log(`Dolphins avarage score is ${dolphinsAvarageScore}, Koalas avarage score is ${koalasAvarageScore}. So winner is: Dolphins team`);
+    } else if (dolphinsAvarageScore < koalasAvarageScore) {
+        console.log(`Dolphins avarage score is ${dolphinsAvarageScore}, Koalas avarage score is ${koalasAvarageScore}. So winner is: Koala's team`);
+    } else {
+        console.log(`Dolphins avarage score is ${dolphinsAvarageScore}, Koalas avarage score is ${koalasAvarageScore}. Both team has same score, so there's no winner`);
+    }
+}
+compareAvarageScores(96, 108, 89, 88, 91, 110);
+compareAvarageScores(97, 112, 101, 109, 95, 123);
+compareAvarageScores(97, 112, 101, 109, 96, 106);
+
 
 
