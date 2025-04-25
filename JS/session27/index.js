ea241e0234972d013403232e308f4978276b14f9
Machakhleli <ushangikakhidze@gmail.com>
2025-04-25 17:33:27 +0400

    done secont task of sesstion 27 tasks

--- JS/session27/index.js
+++ JS/session27/index.js
@@ -6,8 +6,9 @@ const car = {
     year: 2006,
     getInfo: function () {
         return this.brand + " " + this.model + "," + this.year
+        // return car.brand + " " + car.model + "," + car.year
     }
-};
+}
 console.log(car.getInfo());
 console.log("Car brand: ", car.brand);
 
@@ -16,3 +17,14 @@ console.log(car);
 
 delete car.getInfo;
 console.log(car);
+
+// easy task 2
+
+const person = {
+    name: "Usho",
+    age: 35,
+    // greet: function(){ console.log("Hello, my name is " + this.name + " and I am " + this.age + " years old.")}
+    greet: function(){  return "Hello, my name is " + this.name + " and I am " + this.age + " years old."}
+}
+// person.greet();
+console.log(person.greet());
